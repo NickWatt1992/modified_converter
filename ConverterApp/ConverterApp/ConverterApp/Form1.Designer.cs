dc180b67978b644e1a0d289907c0cd14f2efeab7
NickWatt1992 <J221822@tafe.wa.edu.au>
2017-04-07 13:38:23 +0800

    Version 1.2
    Version 1.2

--- ConverterApp/ConverterApp/ConverterApp/Form1.Designer.cs
+++ ConverterApp/ConverterApp/ConverterApp/Form1.Designer.cs
@@ -29,97 +29,165 @@
         private void InitializeComponent()
         {
             this.lbl_UofM = new System.Windows.Forms.Label();
-            this.txt_UnitOfMeasure = new System.Windows.Forms.TextBox();
-            this.btn_CM_to_Inches = new System.Windows.Forms.Button();
-            this.btn_M_to_Feet = new System.Windows.Forms.Button();
             this.btn_Exit = new System.Windows.Forms.Button();
-            this.lbl_Display = new System.Windows.Forms.Label();
-            this.lbl_Convert = new System.Windows.Forms.Label();
-            this.txt_Convert = new System.Windows.Forms.TextBox();
+            this.lbl_CM_TO_I = new System.Windows.Forms.Label();
+            this.lbl_M_TO_FEET = new System.Windows.Forms.Label();
+            this.lbl_CEL_TO_FAH = new System.Windows.Forms.Label();
+            this.lbl_CM_TO_FEET = new System.Windows.Forms.Label();
+            this.lbl_KM_TO_MILES = new System.Windows.Forms.Label();
+            this.btn_ConvertAll = new System.Windows.Forms.Button();
+            this.lb_Convert = new System.Windows.Forms.ListBox();
+            this.tb_CM_to_Inches = new System.Windows.Forms.TextBox();
+            this.tb_M_to_Feet = new System.Windows.Forms.TextBox();
+            this.tb_Cel_to_Fah = new System.Windows.Forms.TextBox();
+            this.tb_CM_to_Feet = new System.Windows.Forms.TextBox();
+            this.tb_KM_to_Miles = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl_UofM
             // 
             this.lbl_UofM.AutoSize = true;
-            this.lbl_UofM.Location = new System.Drawing.Point(12, 24);
+            this.lbl_UofM.Location = new System.Drawing.Point(6, 9);
+            this.lbl_UofM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_UofM.Name = "lbl_UofM";
-            this.lbl_UofM.Size = new System.Drawing.Size(108, 17);
+            this.lbl_UofM.Size = new System.Drawing.Size(90, 13);
             this.lbl_UofM.TabIndex = 0;
-            this.lbl_UofM.Text = "Unit of Measure";
-            // 
-            // txt_UnitOfMeasure
-            // 
-            this.txt_UnitOfMeasure.Location = new System.Drawing.Point(142, 21);
-            this.txt_UnitOfMeasure.Name = "txt_UnitOfMeasure";
-            this.txt_UnitOfMeasure.Size = new System.Drawing.Size(100, 22);
-            this.txt_UnitOfMeasure.TabIndex = 1;
-            // 
-            // btn_CM_to_Inches
-            // 
-            this.btn_CM_to_Inches.Location = new System.Drawing.Point(88, 62);
-            this.btn_CM_to_Inches.Name = "btn_CM_to_Inches";
-            this.btn_CM_to_Inches.Size = new System.Drawing.Size(163, 23);
-            this.btn_CM_to_Inches.TabIndex = 2;
-            this.btn_CM_to_Inches.Text = "Centimetres to Inches";
-            this.btn_CM_to_Inches.UseVisualStyleBackColor = true;
-            this.btn_CM_to_Inches.Click += new System.EventHandler(this.btn_CM_to_Inches_Click);
-            // 
-            // btn_M_to_Feet
-            // 
-            this.btn_M_to_Feet.Location = new System.Drawing.Point(88, 103);
-            this.btn_M_to_Feet.Name = "btn_M_to_Feet";
-            this.btn_M_to_Feet.Size = new System.Drawing.Size(163, 23);
-            this.btn_M_to_Feet.TabIndex = 3;
-            this.btn_M_to_Feet.Text = "Metres to Feet";
-            this.btn_M_to_Feet.UseVisualStyleBackColor = true;
-            this.btn_M_to_Feet.Click += new System.EventHandler(this.btn_M_to_Feet_Click);
+            this.lbl_UofM.Text = "Units of Measure:";
             // 
             // btn_Exit
             // 
-            this.btn_Exit.Location = new System.Drawing.Point(274, 220);
+            this.btn_Exit.Location = new System.Drawing.Point(322, 236);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Exit.Name = "btn_Exit";
-            this.btn_Exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Exit.Size = new System.Drawing.Size(56, 24);
             this.btn_Exit.TabIndex = 4;
-            this.btn_Exit.Text = "EXIT";
+            this.btn_Exit.Text = "Exit";
             this.btn_Exit.UseVisualStyleBackColor = true;
             this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
             // 
-            // lbl_Display
+            // lbl_CM_TO_I
             // 
-            this.lbl_Display.AutoSize = true;
-            this.lbl_Display.Location = new System.Drawing.Point(9, 159);
-            this.lbl_Display.Name = "lbl_Display";
-            this.lbl_Display.Size = new System.Drawing.Size(0, 17);
-            this.lbl_Display.TabIndex = 5;
+            this.lbl_CM_TO_I.AutoSize = true;
+            this.lbl_CM_TO_I.Location = new System.Drawing.Point(6, 33);
+            this.lbl_CM_TO_I.Name = "lbl_CM_TO_I";
+            this.lbl_CM_TO_I.Size = new System.Drawing.Size(109, 13);
+            this.lbl_CM_TO_I.TabIndex = 11;
+            this.lbl_CM_TO_I.Text = "Centimetres to Inches";
             // 
-            // lbl_Convert
+            // lbl_M_TO_FEET
             // 
-            this.lbl_Convert.AutoSize = true;
-            this.lbl_Convert.Location = new System.Drawing.Point(283, 159);
-            this.lbl_Convert.Name = "lbl_Convert";
-            this.lbl_Convert.Size = new System.Drawing.Size(0, 17);
-            this.lbl_Convert.TabIndex = 6;
+            this.lbl_M_TO_FEET.AutoSize = true;
+            this.lbl_M_TO_FEET.Location = new System.Drawing.Point(6, 55);
+            this.lbl_M_TO_FEET.Name = "lbl_M_TO_FEET";
+            this.lbl_M_TO_FEET.Size = new System.Drawing.Size(75, 13);
+            this.lbl_M_TO_FEET.TabIndex = 12;
+            this.lbl_M_TO_FEET.Text = "Metres to Feet";
             // 
-            // txt_Convert
+            // lbl_CEL_TO_FAH
             // 
-            this.txt_Convert.Location = new System.Drawing.Point(202, 156);
-            this.txt_Convert.Name = "txt_Convert";
-            this.txt_Convert.Size = new System.Drawing.Size(71, 22);
-            this.txt_Convert.TabIndex = 7;
+            this.lbl_CEL_TO_FAH.AutoSize = true;
+            this.lbl_CEL_TO_FAH.Location = new System.Drawing.Point(6, 79);
+            this.lbl_CEL_TO_FAH.Name = "lbl_CEL_TO_FAH";
+            this.lbl_CEL_TO_FAH.Size = new System.Drawing.Size(106, 13);
+            this.lbl_CEL_TO_FAH.TabIndex = 13;
+            this.lbl_CEL_TO_FAH.Text = "Celcius to Fahrenheit";
+            // 
+            // lbl_CM_TO_FEET
+            // 
+            this.lbl_CM_TO_FEET.AutoSize = true;
+            this.lbl_CM_TO_FEET.Location = new System.Drawing.Point(6, 103);
+            this.lbl_CM_TO_FEET.Name = "lbl_CM_TO_FEET";
+            this.lbl_CM_TO_FEET.Size = new System.Drawing.Size(98, 13);
+            this.lbl_CM_TO_FEET.TabIndex = 14;
+            this.lbl_CM_TO_FEET.Text = "Centimetres to Feet";
+            // 
+            // lbl_KM_TO_MILES
+            // 
+            this.lbl_KM_TO_MILES.AutoSize = true;
+            this.lbl_KM_TO_MILES.Location = new System.Drawing.Point(6, 127);
+            this.lbl_KM_TO_MILES.Name = "lbl_KM_TO_MILES";
+            this.lbl_KM_TO_MILES.Size = new System.Drawing.Size(94, 13);
+            this.lbl_KM_TO_MILES.TabIndex = 15;
+            this.lbl_KM_TO_MILES.Text = "Kilometres to Miles";
+            // 
+            // btn_ConvertAll
+            // 
+            this.btn_ConvertAll.Location = new System.Drawing.Point(153, 237);
+            this.btn_ConvertAll.Name = "btn_ConvertAll";
+            this.btn_ConvertAll.Size = new System.Drawing.Size(75, 23);
+            this.btn_ConvertAll.TabIndex = 21;
+            this.btn_ConvertAll.Text = "Convert All";
+            this.btn_ConvertAll.UseVisualStyleBackColor = true;
+            this.btn_ConvertAll.Click += new System.EventHandler(this.btn_ConvertAll_Click);
+            // 
+            // lb_Convert
+            // 
+            this.lb_Convert.FormattingEnabled = true;
+            this.lb_Convert.Location = new System.Drawing.Point(9, 149);
+            this.lb_Convert.Name = "lb_Convert";
+            this.lb_Convert.Size = new System.Drawing.Size(369, 82);
+            this.lb_Convert.TabIndex = 22;
+            // 
+            // tb_CM_to_Inches
+            // 
+            this.tb_CM_to_Inches.Location = new System.Drawing.Point(120, 30);
+            this.tb_CM_to_Inches.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_CM_to_Inches.Name = "tb_CM_to_Inches";
+            this.tb_CM_to_Inches.Size = new System.Drawing.Size(207, 20);
+            this.tb_CM_to_Inches.TabIndex = 23;
+            // 
+            // tb_M_to_Feet
+            // 
+            this.tb_M_to_Feet.Location = new System.Drawing.Point(120, 52);
+            this.tb_M_to_Feet.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_M_to_Feet.Name = "tb_M_to_Feet";
+            this.tb_M_to_Feet.Size = new System.Drawing.Size(207, 20);
+            this.tb_M_to_Feet.TabIndex = 24;
+            // 
+            // tb_Cel_to_Fah
+            // 
+            this.tb_Cel_to_Fah.Location = new System.Drawing.Point(120, 76);
+            this.tb_Cel_to_Fah.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Cel_to_Fah.Name = "tb_Cel_to_Fah";
+            this.tb_Cel_to_Fah.Size = new System.Drawing.Size(207, 20);
+            this.tb_Cel_to_Fah.TabIndex = 25;
+            // 
+            // tb_CM_to_Feet
+            // 
+            this.tb_CM_to_Feet.Location = new System.Drawing.Point(120, 100);
+            this.tb_CM_to_Feet.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_CM_to_Feet.Name = "tb_CM_to_Feet";
+            this.tb_CM_to_Feet.Size = new System.Drawing.Size(207, 20);
+            this.tb_CM_to_Feet.TabIndex = 26;
+            // 
+            // tb_KM_to_Miles
+            // 
+            this.tb_KM_to_Miles.Location = new System.Drawing.Point(120, 124);
+            this.tb_KM_to_Miles.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_KM_to_Miles.Name = "tb_KM_to_Miles";
+            this.tb_KM_to_Miles.Size = new System.Drawing.Size(207, 20);
+            this.tb_KM_to_Miles.TabIndex = 27;
             // 
             // frm_Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 255);
-            this.Controls.Add(this.txt_Convert);
-            this.Controls.Add(this.lbl_Convert);
-            this.Controls.Add(this.lbl_Display);
+            this.ClientSize = new System.Drawing.Size(389, 264);
+            this.Controls.Add(this.tb_KM_to_Miles);
+            this.Controls.Add(this.tb_CM_to_Feet);
+            this.Controls.Add(this.tb_Cel_to_Fah);
+            this.Controls.Add(this.tb_M_to_Feet);
+            this.Controls.Add(this.tb_CM_to_Inches);
+            this.Controls.Add(this.lb_Convert);
+            this.Controls.Add(this.btn_ConvertAll);
+            this.Controls.Add(this.lbl_KM_TO_MILES);
+            this.Controls.Add(this.lbl_CM_TO_FEET);
+            this.Controls.Add(this.lbl_CEL_TO_FAH);
+            this.Controls.Add(this.lbl_M_TO_FEET);
+            this.Controls.Add(this.lbl_CM_TO_I);
             this.Controls.Add(this.btn_Exit);
-            this.Controls.Add(this.btn_M_to_Feet);
-            this.Controls.Add(this.btn_CM_to_Inches);
-            this.Controls.Add(this.txt_UnitOfMeasure);
             this.Controls.Add(this.lbl_UofM);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frm_Main";
             this.Text = "ATCA Gas Converter";
             this.ResumeLayout(false);
@@ -130,13 +198,19 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_UofM;
-        private System.Windows.Forms.TextBox txt_UnitOfMeasure;
-        private System.Windows.Forms.Button btn_CM_to_Inches;
-        private System.Windows.Forms.Button btn_M_to_Feet;
         private System.Windows.Forms.Button btn_Exit;
-        private System.Windows.Forms.Label lbl_Display;
-        private System.Windows.Forms.Label lbl_Convert;
-        private System.Windows.Forms.TextBox txt_Convert;
+        private System.Windows.Forms.Label lbl_CM_TO_I;
+        private System.Windows.Forms.Label lbl_M_TO_FEET;
+        private System.Windows.Forms.Label lbl_CEL_TO_FAH;
+        private System.Windows.Forms.Label lbl_CM_TO_FEET;
+        private System.Windows.Forms.Label lbl_KM_TO_MILES;
+        private System.Windows.Forms.Button btn_ConvertAll;
+        private System.Windows.Forms.ListBox lb_Convert;
+        private System.Windows.Forms.TextBox tb_CM_to_Inches;
+        private System.Windows.Forms.TextBox tb_M_to_Feet;
+        private System.Windows.Forms.TextBox tb_Cel_to_Fah;
+        private System.Windows.Forms.TextBox tb_CM_to_Feet;
+        private System.Windows.Forms.TextBox tb_KM_to_Miles;
     }
 }
 
--- ConverterApp/ConverterApp/ConverterApp/Form1.cs
+++ ConverterApp/ConverterApp/ConverterApp/Form1.cs
@@ -9,71 +9,115 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ConverterApp
+
     // This program was written by Gail Mosdell (Modified by Nicholas Watt)
     // It forms the base of a converter program for the OS-Assessment Two for Cert IV
     // Date : February 2017
 {
     public partial class frm_Main : Form
     {
+        // array used to store const doubles
+        double[] convert_Data = new double[5] { 0.3937, 3.28084, 32, 0.0328, 0.621371 };
+
         public frm_Main()
         {
             InitializeComponent();
         }
 
-        // Global Variables and Constants
+        // Provided Global Variables and Constants
         double dbl_UofM, dbl_Convert;
+
 
         private void btn_Exit_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        private void btn_CM_to_Inches_Click(object sender, EventArgs e)
+        private void Convert_to_CMtoI()
         {
-            const double CM_TO_INCH = 0.3937;
-           
             // validate user entry and convert to a double
-
-            if (! double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            if (!double.TryParse(tb_CM_to_Inches.Text, out dbl_UofM))
             {
-                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
-                txt_UnitOfMeasure.Clear();
-                txt_UnitOfMeasure.Focus();
-                txt_Convert.Clear();
-                lbl_Convert.Text = "";
-                lbl_Display.Text = "";
+                MessageBox.Show("A numeric must be entered. Please enter a valid number of Centimetres.");
+                tb_CM_to_Inches.Clear();
+                tb_CM_to_Inches.Focus();
             }
             else
             {
-                dbl_Convert = dbl_UofM * CM_TO_INCH;
-                txt_Convert.Text = dbl_Convert.ToString();
-                lbl_Display.Text = txt_UnitOfMeasure.Text + " centimetres is converted to ";
-                lbl_Convert.Text = " inches.";
+                dbl_Convert = dbl_UofM * convert_Data[0];
+                lb_Convert.Items.Add(dbl_Convert + " Inches");
             }
         }
-
-        private void btn_M_to_Feet_Click(object sender, EventArgs e)
+        private void Convert_M_to_Feet()
         {
-         const double M_TO_FEET = 3.28084;
-           
             // validate user entry and convert to a double
-
-            if (! double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            if (!double.TryParse(tb_M_to_Feet.Text, out dbl_UofM))
             {
-                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
-                txt_UnitOfMeasure.Clear();
-                txt_UnitOfMeasure.Focus();
-                txt_Convert.Clear();
-                lbl_Convert.Text = "";
-                lbl_Display.Text = "";
+                MessageBox.Show("A numeric must be entered. Please enter a valid number of Metres.");
+                tb_M_to_Feet.Clear();
+                tb_M_to_Feet.Focus();
             }
             else
             {
-                dbl_Convert = dbl_UofM* M_TO_FEET;
-                txt_Convert.Text = dbl_Convert.ToString();
-                lbl_Display.Text = txt_UnitOfMeasure.Text + " metres is converted to ";
-                lbl_Convert.Text = " feet.";
+                dbl_Convert = dbl_UofM * convert_Data[1];
+                lb_Convert.Items.Add(dbl_Convert + " Feet");
             }
+        }
+        private void Convert_Cel_to_Fah()
+        {
+            // validate user entry and convert to a double
+            if (!double.TryParse(tb_Cel_to_Fah.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please enter a valid degree of Celsius.");
+                tb_Cel_to_Fah.Clear();
+                tb_Cel_to_Fah.Focus();
+            }
+            else
+            {
+                dbl_Convert = dbl_UofM  * 9 / 5 + convert_Data[2];
+                lb_Convert.Items.Add(dbl_Convert + " Fahrenheit");
+            }
+        }
+        private void Convert_CM_to_Feet()
+        {
+            // validate user entry and convert to a double
+            if (!double.TryParse(tb_CM_to_Feet.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please enter a valid number of Centimetres.");
+                tb_CM_to_Feet.Clear();
+                tb_CM_to_Feet.Focus();
+            }
+            else
+            {
+                dbl_Convert = dbl_UofM * convert_Data[3];
+                lb_Convert.Items.Add(dbl_Convert + " Feet");
+            }
+        }
+        private void Convert_KM_TO_Miles()
+        {
+            // validate user entry and convert to a double
+            if (!double.TryParse(tb_KM_to_Miles.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please enter a valid number of Kilometres.");
+                tb_KM_to_Miles.Clear();
+                tb_KM_to_Miles.Focus();
+            }
+            else
+            {
+                dbl_Convert = dbl_UofM * convert_Data[4];
+                lb_Convert.Items.Add(dbl_Convert + " Miles");
+            }
+        }
+        // Displays all conversions in a listbox
+        private void btn_ConvertAll_Click(object sender, EventArgs e)
+        {
+            lb_Convert.Items.Clear();
+
+            Convert_to_CMtoI();
+            Convert_M_to_Feet();
+            Convert_Cel_to_Fah();
+            Convert_CM_to_Feet();
+            Convert_KM_TO_Miles();
         }
     }
 }
